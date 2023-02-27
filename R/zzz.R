3b7396d519f26e50a35dd63145ce6989c892f2e6
Aleš Vomáčka <alesvomacka@seznam.cz>
2023-02-27 13:48:43 +0100

    Specified path to fonts using system.file

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
 
-  sysfonts::font_paths("inst/fonts")
+  sysfonts::font_paths(system.file("fonts", package = "avom"))
   sysfonts::font_add(family = "Fira Sans",
                      regular = "FiraSans-Regular.ttf",
                      bold = "FiraSans-Bold.ttf",
