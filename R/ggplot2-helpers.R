64d9c10b2e722698be52aa2a439b841b09748d81
Aleš Vomáčka <61730945+alesvomacka@users.noreply.github.com>
2023-12-25 23:19:53 +0100

    added avom_geom_defaults()

--- R/ggplot2-helpers.R
+++ R/ggplot2-helpers.R
@@ -34,3 +34,31 @@ avom_ggsave <- function(filename,
                   device = device,
                   ...)
 }
+
+#' Quickly set geoms defaults
+#'
+#' Sets color and font family for common geoms.
+#'
+#' @param def_color Default color for columns, points, lines, etc.
+#' @param font_family Default font family for text.
+#' @param font_color Default font color for text.
+#'
+#' @export
+#'
+#' @examples
+#' avom_ggplot2_defaults()
+avom_ggplot2_defaults <- function(def_color = "#83a598",
+                                  font_family = "Fira Sans",
+                                  font_color = "#282828"){
+  # Colors and fills
+  ggplot2::update_geom_defaults("col", list(fill = def_color))
+  ggplot2::update_geom_defaults("ribbon", list(fill = def_color))
+  ggplot2::update_geom_defaults("point", list(colour = def_color))
+  ggplot2::update_geom_defaults("line", list(colour = def_color))
+  ggplot2::update_geom_defaults("smooth", list(colour = def_color))
+  ggplot2::update_geom_defaults("pointrange", list(colour = def_color))
+  ggplot2::update_geom_defaults("boxplot", list(colour = def_color))
+  # Text
+  ggplot2::update_geom_defaults("text", list(family = font_family,
+                                             color = font_color))
+}
--- R/ggplot2-themes.R
+++ R/ggplot2-themes.R
@@ -5,6 +5,9 @@
 #'
 #' @param font_family Font family. Defaults to "Fira Sans".
 #' @param font_size Baseline size for text. Defaults to 10.
+#' @param font_color Font color. Defaults to "#282828".
+#' @param backround_color Backround color. Defaults to "#fbf1c7".
+#' @param gridlines_color Color of gridlines. Defaults to "#d1c8c0".
 #' @param ... Other arguments passed to [ggplot2::theme_minimal()].
 #' @export
 #'
@@ -15,12 +18,17 @@
 #'          theme_avom()
 #'          }
 
-theme_avom <- function(font_family = "Fira Sans", font_size = 10, ...){
+theme_avom <- function(font_family = "Fira Sans", font_size = 10,
+                       font_color = "#282828",
+                       backround_color = "#fbf1c7",
+                       gridlines_color = "#d1c8c0",
+                       ...){
   ggplot2::theme_minimal() +
     ggplot2::theme(
       # Text
       text = ggplot2::element_text(family = font_family,
-                                   size = font_size),
+                                   size = font_size,
+                                   color = font_color),
       plot.title = ggtext::element_textbox_simple(size = ggplot2::rel(1.3)),
       plot.title.position = "plot",
       plot.caption.position = "plot",
@@ -32,11 +40,11 @@ theme_avom <- function(font_family = "Fira Sans", font_size = 10, ...){
       plot.caption = ggtext::element_markdown(hjust = 0,
                                               size = ggplot2::rel(0.8)),
       # Backround
-      panel.background = ggplot2::element_rect(fill = "#fbf1c7", colour = NA),
-      plot.background = ggplot2::element_rect(fill = "#fbf1c7"),
+      panel.background = ggplot2::element_rect(fill = backround_color, colour = NA),
+      plot.background = ggplot2::element_rect(fill = backround_color),
       # Grid Lines
       panel.grid.minor = ggplot2::element_blank(),
-      panel.grid.major = ggplot2::element_line(colour = "#d1c8c0",
+      panel.grid.major = ggplot2::element_line(colour = gridlines_color,
                                                linetype = "dashed"),
       ...)
 }
